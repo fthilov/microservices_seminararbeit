ffc5f97cedf89b08b0908ba1e7db135ad30a5a7f
Felix von dem Berge <felixvdb19@gmail.com>
2024-11-18 15:09:43 +0100

    Formatting rec_controller

--- Aufgabe 5/rec_controller/index-skeleton.js
+++ Aufgabe 5/rec_controller/index-skeleton.js
@@ -1,8 +1,6 @@
 var mqtt = require('mqtt');
 var Topic = 'SWS/#'; //subscribe to all topics from postapp
 
-
-
 var client  = mqtt.connect('mqtt://broker.hivemq.com');
 
 client.on('connect', mqtt_connect);
@@ -11,14 +9,12 @@ client.on('error', mqtt_error);
 client.on('message', mqtt_messsageReceived);
 client.on('close', mqtt_close);
 
-function mqtt_connect()
-{
+function mqtt_connect() {
     console.log("Connecting MQTT");
     client.subscribe(Topic, mqtt_subscribe);
 }
 
-function mqtt_subscribe(err, granted)
-{
+function mqtt_subscribe(err, granted) {
     console.log("Subscribed to " + Topic);
     if (err) {console.log(err);}
 }
@@ -29,25 +25,21 @@ function mqtt_reconnect(err)
     if (err) {console.log(err);}
 }
 
-function mqtt_error(err)
-{
+function mqtt_error(err) {
     console.log("Error!");
 	if (err) {console.log(err);}
 }
 
-function after_publish()
-{
+function after_publish() {
 	//do nothing
 }
 
-function mqtt_messsageReceived(topic, message, packet)
-{
+function mqtt_messsageReceived(topic, message, packet) {
 
     console.log('Topic=' +  topic + '  Message=' + message);
 }
 
-function mqtt_close(err)
-{
+function mqtt_close(err) {
 	console.log("Close MQTT");
     if (err) {console.log(err);}
 }
